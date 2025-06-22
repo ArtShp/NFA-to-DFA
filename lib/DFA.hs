f7c75b030c185975840195b895abd73cc2952f59
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-06-22 19:26:04 +0200

    feat: add DFA validator
    It returns ValidDFA object if DFA is valid, otherwise nothing. We need it to be sure we're working with a correct one -> no exceptions

--- lib/DFA.hs
+++ lib/DFA.hs
@@ -22,3 +22,19 @@ data (Show q, Show a) => ValidDFA q a = ValidDFA
     , initialState :: q
     , finalStates  :: States q
     }
+
+validateDFA :: (Eq q, Show q, Show a) => DFA q a -> Maybe (ValidDFA q a)
+validateDFA dfa@(DFA states alphabet transition initialState finalStates) =
+    if isValidDFA dfa
+    then Just (ValidDFA states alphabet transition initialState finalStates)
+    else Nothing
+
+isValidDFA :: (Eq q, Show q, Show a) => DFA q a -> Bool
+isValidDFA (DFA states alphabet transition initialState finalStates) =
+    not (null states) &&
+    not (null alphabet) &&
+    initialState `elem` states &&
+    all (`elem` states) finalStates &&
+    all (\q -> all (\a -> case transition q a of
+                            Just nextState -> nextState `elem` states
+                            Nothing -> False) alphabet) states
