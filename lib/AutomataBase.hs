6024d66904153d422dab2ae9f52de815372e2612
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-06-22 18:46:20 +0200

    feat: add automata basic components
    Add types for States and Alphabet

---
+++ lib/AutomataBase.hs
@@ -0,0 +1,9 @@
+module AutomataBase where
+
+-- #TODO: Maybe it's better to use 
+-- newtype States q = States [q], or even data
+type States q = [q]
+
+-- #TODO: Maybe it's better to use 
+-- newtype Alphabet a = Alphabet [a], or even data
+type Alphabet a = [a]
