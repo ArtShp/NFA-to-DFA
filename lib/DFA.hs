760557df81b1e21e16a82c19356dc4197d17b742
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-06-22 19:23:05 +0200

    feat: add type for valid DFA

--- lib/DFA.hs
+++ lib/DFA.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
 module DFA where
 
 import AutomataBase
@@ -5,6 +8,14 @@ import AutomataBase
 type DFATransition q a = q -> a -> Maybe q
 
 data (Show q, Show a) => DFA q a = DFA
+    { states       :: States q
+    , alphabet     :: Alphabet a
+    , transition   :: DFATransition q a
+    , initialState :: q
+    , finalStates  :: States q
+    }
+
+data (Show q, Show a) => ValidDFA q a = ValidDFA
     { states       :: States q
     , alphabet     :: Alphabet a
     , transition   :: DFATransition q a
