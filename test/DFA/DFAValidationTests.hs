f2ab74b121f77d99b95ff9667b684d359d609e42
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-06-27 17:36:53 +0200

    test!: update DFA validation tests
    Correct them and uncomment as DFA API changed

--- test/DFA/DFAValidationTests.hs
+++ test/DFA/DFAValidationTests.hs
@@ -1,42 +1,61 @@
 module DFA.DFAValidationTests (dfaValidationTests) where
 
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+
 import Test.HUnit
--- import Data.Maybe (isJust, isNothing)
--- import DFA
+import Data.Maybe (isJust, isNothing)
+import DFA
+
+-- Common pieces for tests:
+states_ :: Set.Set Int
+states_ = Set.fromList [0,1]
+
+alphabet_ :: Set.Set Char
+alphabet_ = Set.fromList "ab"
+
+validTrans :: DFATransition Int Char
+validTrans = Map.fromList
+    [((s, c), s)
+    | s <- Set.toList states_, c <- Set.toList alphabet_
+    ]
+
+initial :: Int
+initial = 0
+
+finals :: Set.Set Int
+finals = Set.fromList [1]
 
 dfaValidationTests :: [Test]
-dfaValidationTests = []
-    -- [ TestLabel "valid simple DFA"             testValidDFA
-    -- , TestLabel "empty states"                 testEmptyStates
-    -- , TestLabel "empty alphabet"               testEmptyAlphabet
-    -- , TestLabel "initial not in states"        testInitialNotInStates
-    -- , TestLabel "final not a subset of states" testFinalNotSubset
-    -- ]
+dfaValidationTests =
+    [ TestLabel "valid simple DFA"           testValidDFA
+    , TestLabel "empty states"               testEmptyStates
+    , TestLabel "empty alphabet"             testEmptyAlphabet
+    , TestLabel "initial not in states"      testInitialNotInStates
+    , TestLabel "final not subset of states" testFinalNotSubset
+    ]
 
--- simpleLoop :: Int -> Char -> Maybe Int
--- simpleLoop q _ = Just q
+testValidDFA :: Test
+testValidDFA = TestCase $
+    assertBool "makeDFA should succeed for a simple valid DFA"
+        (isJust (makeDFA states_ alphabet_ validTrans initial finals))
 
--- testValidDFA :: Test
--- testValidDFA = TestCase $
---     assertBool "makeDFA should succeed for a simple valid DFA"
---         (isJust (makeDFA [0, 1] "ab" simpleLoop 0 [1]))
+testEmptyStates :: Test
+testEmptyStates = TestCase $
+    assertBool "makeDFA should fail on empty states"
+        (isNothing (makeDFA Set.empty alphabet_ validTrans initial finals))
 
--- testEmptyStates :: Test
--- testEmptyStates = TestCase $
---     assertBool "makeDFA should fail on empty states"
---         (isNothing (makeDFA [] "a" simpleLoop 0 []))
+testEmptyAlphabet :: Test
+testEmptyAlphabet = TestCase $
+    assertBool "makeDFA should fail on empty alphabet"
+        (isNothing (makeDFA states_ Set.empty validTrans initial finals))
 
--- testEmptyAlphabet :: Test
--- testEmptyAlphabet = TestCase $
---     assertBool "makeDFA should fail on empty alphabet"
---         (isNothing (makeDFA [0] "" simpleLoop 0 [0]))
+testInitialNotInStates :: Test
+testInitialNotInStates = TestCase $
+    assertBool "makeDFA should fail if initialState is not in states"
+        (isNothing (makeDFA states_ alphabet_ validTrans 42 finals))
 
--- testInitialNotInStates :: Test
--- testInitialNotInStates = TestCase $
---     assertBool "makeDFA should fail if initialState is not in states"
---         (isNothing (makeDFA [1] "a" simpleLoop 0 [1]))
-
--- testFinalNotSubset :: Test
--- testFinalNotSubset = TestCase $
---     assertBool "makeDFA should fail if finalStates is not a subset of states"
---         (isNothing (makeDFA [0, 1, 2] "a" simpleLoop 0 [1, 3]))
+testFinalNotSubset :: Test
+testFinalNotSubset = TestCase $
+    assertBool "makeDFA should fail if finalStates is not a subset of states"
+        (isNothing (makeDFA states_ alphabet_ validTrans initial (Set.fromList [2])))
