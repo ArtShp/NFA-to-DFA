a3ff418e5c01c715d02e3d4ed6082a6cece042b8
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-06-27 22:27:23 +0200

    feat!: add NFA simulators tests

--- test/NFA/NFATests.hs
+++ test/NFA/NFATests.hs
@@ -3,6 +3,7 @@ module NFA.NFATests (nfaTests) where
 import Test.HUnit
 
 import NFA.NFAValidationTests
+import NFA.NFASimulatorsTests
 
 nfaTests :: [Test]
-nfaTests = nfaValidationTests
+nfaTests = nfaValidationTests ++ nfaSimulatorsTests
