362493d24f3fea3c3ba1b356be0fbe30260355a6
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-06-28 16:51:56 +0200

    feat: add expected DFAs for NFAs

--- lib/TestData/NFA_1.hs
+++ lib/TestData/NFA_1.hs
@@ -4,14 +4,27 @@ module TestData.NFA_1 (
   , nfaInputs_1
   , nfaOutputs_1
   , nfaIO_1
+  , dfaExpected_1
+  , dfaExpectedTrans_1
 ) where
 
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
+import AutomataBase
+import qualified DFA
 import qualified NFA
 
 -- ==================== BEGIN - NFA 1 ====================
+
+nfa_1 :: NFA.NFA Int Char
+nfa_1 = NFA.NFA
+    { NFA.states       = Set.fromList [0 .. 9]
+    , NFA.alphabet     = Set.fromList "ab"
+    , NFA.transition   = nfaTrans_1
+    , NFA.initialState = 0
+    , NFA.finalStates  = Set.singleton 9
+    }
 
 nfaTrans_1 :: NFA.NFATransition Int Char
 nfaTrans_1 = Map.fromList
@@ -26,14 +39,7 @@ nfaTrans_1 = Map.fromList
     , ((8, Nothing), Set.singleton 9)
     ]
 
-nfa_1 :: NFA.NFA Int Char
-nfa_1 = NFA.NFA
-    { NFA.states       = Set.fromList [0 .. 9]
-    , NFA.alphabet     = Set.fromList "ab"
-    , NFA.transition   = nfaTrans_1
-    , NFA.initialState = 0
-    , NFA.finalStates  = Set.singleton 9
-    }
+
 
 nfaInputs_1 :: [String]
 nfaInputs_1 = ["", "a", "aa", "aaaa"]
@@ -43,5 +49,25 @@ nfaOutputs_1 = [False, False, True, False]
 
 nfaIO_1 :: [(String, Bool)]
 nfaIO_1 = zip nfaInputs_1 nfaOutputs_1
+
+
+
+dfaExpected_1 :: DFA.DFA (States Int) Char
+dfaExpected_1 = DFA.DFA
+    { DFA.states       = Set.fromList [Set.fromList [0], Set.fromList [1,2,4], Set.fromList [3,9], Set.fromList [5,6,8,9], Set.fromList [6,7,8,9]]
+    , DFA.alphabet     = Set.fromList "ab"
+    , DFA.transition   = dfaExpectedTrans_1
+    , DFA.initialState = Set.singleton 0
+    , DFA.finalStates  = Set.fromList [Set.fromList [3,9], Set.fromList [5,6,8,9], Set.fromList [6,7,8,9]]
+    }
+
+dfaExpectedTrans_1 :: DFA.DFATransition (States Int) Char
+dfaExpectedTrans_1 = Map.fromList
+    [ ((Set.fromList [0], 'a'), Set.fromList [1,2,4])
+    , ((Set.fromList [1,2,4], 'a'), Set.fromList [3,9])
+    , ((Set.fromList [1,2,4], 'b'), Set.fromList [5,6,8,9])
+    , ((Set.fromList [5,6,8,9], 'b'), Set.fromList [6,7,8,9])
+    , ((Set.fromList [6,7,8,9], 'b'), Set.fromList [6,7,8,9])
+    ]
 
 -- ==================== END - NFA 1 ====================
--- lib/TestData/NFA_2.hs
+++ lib/TestData/NFA_2.hs
@@ -4,15 +4,27 @@ module TestData.NFA_2 (
   , nfaInputs_2
   , nfaOutputs_2
   , nfaIO_2
+  , dfaExpected_2
+  , dfaExpectedTrans_2
 ) where
 
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
-import AutomataBase (States)
+import AutomataBase
+import qualified DFA
 import qualified NFA
 
 -- ==================== BEGIN - NFA 2 ====================
+
+nfa_2 :: NFA.NFA Int Char
+nfa_2 = NFA.NFA
+    { NFA.states       = Set.fromList [0 .. 5]
+    , NFA.alphabet     = Set.fromList "0123456789+-."
+    , NFA.transition   = nfaTrans_2
+    , NFA.initialState = 0
+    , NFA.finalStates  = Set.singleton 5
+    }
 
 nfaTrans_2 :: NFA.NFATransition Int Char
 nfaTrans_2 = Map.fromList (
@@ -27,18 +39,7 @@ nfaTrans_2 = Map.fromList (
     transDigit 2 [3] ++
     transDigit 3 [3])
 
-nfa_2 :: NFA.NFA Int Char
-nfa_2 = NFA.NFA
-    { NFA.states       = Set.fromList [0 .. 5]
-    , NFA.alphabet     = Set.fromList "0123456789+-."
-    , NFA.transition   = nfaTrans_2
-    , NFA.initialState = 0
-    , NFA.finalStates  = Set.singleton 5
-    }
 
-transDigit :: Int -> [Int] -> [((Int, Maybe Char), States Int)]
-transDigit from tos = 
-    [((from, Just c), Set.fromList tos) | c <- ['0' .. '9']]
 
 nfaInputs_2 :: [String]
 nfaInputs_2 = ["12.34", "-1.", "+.555", ".", "13249", "--12.12"]
@@ -49,4 +50,39 @@ nfaOutputs_2 = [True, True, True, False, False, False]
 nfaIO_2 :: [(String, Bool)]
 nfaIO_2 = zip nfaInputs_2 nfaOutputs_2
 
+
+
+dfaExpected_2 :: DFA.DFA (States Int) Char
+dfaExpected_2 = DFA.DFA
+    { DFA.states       = Set.fromList [Set.fromList [0,1], Set.fromList [1], Set.fromList [1,4], Set.fromList [2], Set.fromList [2,3,5], Set.fromList [3,5]]
+    , DFA.alphabet     = Set.fromList "0123456789+-."
+    , DFA.transition   = dfaExpectedTrans_2
+    , DFA.initialState = Set.fromList [0,1]
+    , DFA.finalStates  = Set.fromList [Set.fromList [2,3,5], Set.fromList [3,5]]
+    }
+
+dfaExpectedTrans_2 :: DFA.DFATransition (States Int) Char
+dfaExpectedTrans_2 = Map.fromList $ concat
+    [ trans1 [0,1] [1,4] "0123456789"
+    , trans1 [0,1] [1] "+-"
+    , trans1 [0,1] [2] "."
+    , trans1 [1] [2] "."
+    , trans1 [1] [1,4] "0123456789"
+    , trans1 [1,4] [2,3,5] "."
+    , trans1 [1,4] [1,4] "0123456789"
+    , trans1 [2] [3,5] "0123456789"
+    , trans1 [2,3,5] [3,5] "0123456789"
+    , trans1 [3,5] [3,5] "0123456789"
+    ]
+
 -- ==================== END - NFA 2 ====================
+
+-- ==================== MISC ====================
+
+transDigit :: Int -> [Int] -> [((Int, Maybe Char), States Int)]
+transDigit from tos = 
+    [((from, Just c), Set.fromList tos) | c <- ['0' .. '9']]
+
+trans1 :: [Int] -> [Int] -> [Char] -> [((States Int, Char), States Int)]
+trans1 xs ys as =
+    [((Set.fromList xs, c), Set.fromList ys) | c <- as]
