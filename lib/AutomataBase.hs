3af87bfbc26b182d1004def7fb20730096818225
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-06-28 17:29:53 +0200

    refactor: delete TODO comments as unused

--- lib/AutomataBase.hs
+++ lib/AutomataBase.hs
@@ -2,12 +2,8 @@ module AutomataBase where
 
 import qualified Data.Set as Set
 
--- #TODO: Maybe it's better to use 
--- newtype States q = States [q], or even data
 type States q = Set.Set q
 
--- #TODO: Maybe it's better to use 
--- newtype Alphabet a = Alphabet [a], or even data
 type Alphabet a = Set.Set a
 
 showStringListNoBrackets :: [String] -> String
