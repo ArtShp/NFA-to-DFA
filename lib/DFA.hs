1a67742693406bb652e5cefa77333e0f163f2826
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-06-28 11:37:34 +0200

    feat!: add show methods for DFA

--- lib/DFA.hs
+++ lib/DFA.hs
@@ -18,7 +18,28 @@ data DFA q a = DFA
     , transition   :: DFATransition q a
     , initialState :: q
     , finalStates  :: States q
-    } deriving (Show, Eq)
+    } deriving (Eq)
+
+instance (Ord q, Ord a, Show q, Show a) => Show (DFA q a) where
+    show DFA{states = qs, alphabet = as, transition = trans, initialState = q0, finalStates = fs} =
+        "DFA { " ++ "\n" ++
+        "    states = " ++ showStates qs ++ "\n" ++
+        "    alphabet = " ++ showAlphabet as ++ "\n" ++
+        "    transition = " ++ showDFATransition trans ++ "\n" ++
+        "    initialState = " ++ show q0 ++ "\n" ++
+        "    finalStates = " ++ showStates fs ++ "\n" ++
+        "}"
+
+showDFATransition :: (Ord q, Ord a, Show q, Show a) => DFATransition q a -> String
+showDFATransition trans =
+    "{" ++ "\n" ++
+    concat [ tab2 ++ showRule (q, a) nextState ++ "\n"
+           | ((q, a), nextState) <- Map.toList trans
+           ] ++ tab ++ "}"
+    where
+        showRule (q, a) next = "(" ++ show q ++ ", " ++ show a ++ ") -> " ++ show next
+        tab = "                 "
+        tab2 = tab ++ "  "
 
 isValidDFA :: (Ord q, Ord a) => DFA q a -> Bool
 isValidDFA (DFA qs as trans q0 fs) =
